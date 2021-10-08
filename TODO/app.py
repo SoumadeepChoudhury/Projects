e5a8ac1fd77ac53658bcb1a6cbc2b354f5eb02e9
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-10-09 00:54:37 +0530

    Flask App updated. Added functionalities.

--- TODO/app.py
+++ TODO/app.py
@@ -1,7 +1,34 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import defaultload
+from datetime import datetime as dt
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_Modified = db.Column(db.Date, default=dt.now)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} {self.title} {self.date_Modified}"
+
+
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

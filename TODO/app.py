a455785f90ccf4ec6c1c8c52407a8234d971b252
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-11-04 11:10:16 +0530

    Updated app

--- TODO/app.py
+++ TODO/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import defaultload
 from datetime import datetime as dt
